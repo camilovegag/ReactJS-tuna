63bd3d370c1350989031c7c01f20b05046062f82
Camilo Vega <camilovegag@unisabana.edu.co>
2020-02-16 16:35:58 -0500

    created footer and put some social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,17 @@
 import React from "react";
+import SocialMediaButton from "./buttons/SocialMediaButton";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="socialContainer">
+        <SocialMediaButton />
+        <SocialMediaButton />
+        <SocialMediaButton />
+        <SocialMediaButton />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/buttons/SocialMediaButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SocialMediaButton(props) {
+  return (
+    <div className="redes">
+      <i className="fab fa-facebook-f"></i>
+    </div>
+  );
+}
+
+export default SocialMediaButton;
