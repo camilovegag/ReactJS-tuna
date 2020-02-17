1170748bd1946e3fe0b4955ab47381d99b532586
Carlosayol <60300191+Carlosayol@users.noreply.github.com>
2020-02-17 18:43:27 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,8 +12,6 @@ function Home() {
           <p className="descripcion">Lorem ipsum dolor sit amet, sed inv enire repudiare ne, habemus vulput ate sed ei. Vix impedit.</p>
         </div>
         <img className="tunos" src={Tunos} alt="grafico de tunos"></img>
-        <Contact titulo="Contactanos" displaytext="¿Quieres una serenata de la Tuna? Llena los datos del formulario de abajo para entrar en contacto" fstinput="Tu nombre" 
-        scninput="Tu correo" msginput="Tu mensaje" buttontext="Entra en contacto" />
       </div>
     </div>
   );
